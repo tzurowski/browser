49c0c4dab20a73bce679e49e389c607ccd87ade6
Piotr Płatos <piotr.platos97@gmail.com>
2019-01-15 16:40:01 +0100

    Begin version of design

--- Browser/Browser/Form1.Designer.cs
+++ Browser/Browser/Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.back_btn = new System.Windows.Forms.Button();
+            this.next_btn = new System.Windows.Forms.Button();
+            this.address_bar_textbos = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPrivateWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findOnThisWebsiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // back_btn
+            // 
+            this.back_btn.Location = new System.Drawing.Point(0, 0);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(58, 33);
+            this.back_btn.TabIndex = 0;
+            this.back_btn.Text = "<-";
+            this.back_btn.UseVisualStyleBackColor = true;
+            // 
+            // next_btn
+            // 
+            this.next_btn.Location = new System.Drawing.Point(64, 0);
+            this.next_btn.Name = "next_btn";
+            this.next_btn.Size = new System.Drawing.Size(58, 33);
+            this.next_btn.TabIndex = 1;
+            this.next_btn.Text = "->";
+            this.next_btn.UseVisualStyleBackColor = true;
+            // 
+            // address_bar_textbos
+            // 
+            this.address_bar_textbos.Location = new System.Drawing.Point(130, 5);
+            this.address_bar_textbos.Name = "address_bar_textbos";
+            this.address_bar_textbos.Size = new System.Drawing.Size(1049, 22);
+            this.address_bar_textbos.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 33);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1259, 632);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(1207, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(64, 660);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.historyToolStripMenuItem,
+            this.newWindowToolStripMenuItem,
+            this.newPrivateWindowToolStripMenuItem,
+            this.downloadFilesToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.findOnThisWebsiteToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(217, 6);
+            // 
+            // historyToolStripMenuItem
+            // 
+            this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.historyToolStripMenuItem.Text = "History";
+            // 
+            // newWindowToolStripMenuItem
+            // 
+            this.newWindowToolStripMenuItem.Name = "newWindowToolStripMenuItem";
+            this.newWindowToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.newWindowToolStripMenuItem.Text = "New window";
+            // 
+            // newPrivateWindowToolStripMenuItem
+            // 
+            this.newPrivateWindowToolStripMenuItem.Name = "newPrivateWindowToolStripMenuItem";
+            this.newPrivateWindowToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.newPrivateWindowToolStripMenuItem.Text = "New private window";
+            // 
+            // downloadFilesToolStripMenuItem
+            // 
+            this.downloadFilesToolStripMenuItem.Name = "downloadFilesToolStripMenuItem";
+            this.downloadFilesToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.downloadFilesToolStripMenuItem.Text = "Download files";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // findOnThisWebsiteToolStripMenuItem
+            // 
+            this.findOnThisWebsiteToolStripMenuItem.Name = "findOnThisWebsiteToolStripMenuItem";
+            this.findOnThisWebsiteToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.findOnThisWebsiteToolStripMenuItem.Text = "Find on this website";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(220, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1271, 660);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.address_bar_textbos);
+            this.Controls.Add(this.next_btn);
+            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button back_btn;
+        private System.Windows.Forms.Button next_btn;
+        private System.Windows.Forms.TextBox address_bar_textbos;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newPrivateWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem downloadFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findOnThisWebsiteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
     }
 }
 
